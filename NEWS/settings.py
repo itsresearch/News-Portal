2a8d922a66b6d9e5ac1adb031ea732b132ea2a26
Research Devkota <researchofficial55@gmail.com>
2025-06-02 12:02:39 +0545

    Second push

--- NEWS/settings.py
+++ NEWS/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'NEWS.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ["templates"],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -116,7 +116,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = ["static"]
 
+MEDIA_URL = "/media/"
+MEDIA_ROOT = "media"
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
