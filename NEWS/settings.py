9cf28b438fae3382d4f7c3ad9eacd8e56e266693
Research Devkota <researchofficial55@gmail.com>
2025-06-19 21:02:31 +0545

    Serializers added.

--- NEWS/settings.py
+++ NEWS/settings.py
@@ -31,14 +31,19 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    #built-in apps
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # 3rd-party apps
+    'rest_framework',
+    # local apps
     'newspaper',
     'accounts',
+    'api',
 ]
 
 MIDDLEWARE = [
@@ -129,3 +134,8 @@ MEDIA_ROOT = "media"
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = "/"
 LOGOUT_REDIRECT_URL = "/"
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
+}
--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
 from . import views
-from .forms import LoginForm  # Import the custom login form
+from .forms import LoginForm  
 
 app_name = "accounts"
 
@@ -10,7 +10,7 @@ urlpatterns = [
         "login/",
         auth_views.LoginView.as_view(
             template_name="registration/login.html", 
-            authentication_form=LoginForm  # Note: Fixed typo (was authentication_from)
+            authentication_form=LoginForm  
         ),
         name="login",
     ),
