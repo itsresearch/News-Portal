68e87ca2541700f18cc387c869c87f1e31579a3a
Research Devkota <134274596+itsresearch@users.noreply.github.com>
2025-11-21 10:50:22 +0545

    Update ALLOWED_HOSTS for production settings

--- NEWS/settings.py
+++ NEWS/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-+q))w)t1^-oho%65-*3h_hiz6$*pqcyao*a4mp%jkc=fj0soix
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["news-portal-iidc.onrender.com", "localhost"]
 
 
 # Application definition
